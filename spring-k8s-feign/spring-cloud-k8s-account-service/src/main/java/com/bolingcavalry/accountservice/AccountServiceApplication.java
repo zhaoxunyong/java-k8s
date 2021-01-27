3d8d1c342279b690430dd6035e1b36a94d6baed5
dave.zhao <dave.zhao@zerofinance.com>
2021-01-27 10:12:46 +0800

    xxxx

--- spring-k8s-feign/spring-cloud-k8s-account-service/src/main/java/com/bolingcavalry/accountservice/AccountServiceApplication.java
+++ spring-k8s-feign/spring-cloud-k8s-account-service/src/main/java/com/bolingcavalry/accountservice/AccountServiceApplication.java
@@ -1,10 +1,22 @@
 package com.bolingcavalry.accountservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AccountServiceApplication {
+
+    @Value("${myvalue}")
+    private String myvalue;
+
+    @GetMapping("/test")
+    public String test() {
+        return myvalue;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
