cb04bcd8c4f3c223b9cf86b0e5e3c5130b492281
dave.zhao <dave.zhao@zerofinance.com>
2021-02-08 17:57:06 +0800

    kubernetes-maven-plugin

--- spring-k8s-feign/spring-cloud-k8s-account-service/k8sbuild.sh
+++ spring-k8s-feign/spring-cloud-k8s-account-service/k8sbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# mvn clean install -Dmaven.test.skip=true -Dspring.profile=dev -Pk8s
-mvn clean install k8s:build k8s:push k8s:resource k8s:deploy -Dmaven.test.skip=true -Dspring.profile=dev -Pk8s
+mvn clean install -Dmaven.test.skip=true -Dspring.profile=dev -Pk8s
+# mvn clean install k8s:build k8s:push k8s:resource k8s:apply -Dmaven.test.skip=true -Dspring.profile=dev -Pk8s
